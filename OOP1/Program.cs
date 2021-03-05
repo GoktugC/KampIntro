5ab47bdbb90133b5dbdac9b6854e40b1c47acf40
Goktug <goktug1801@gmail.com>
2021-03-05 17:38:59 +0300

    5M21

---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Can be written in both ways.
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.CategoryID = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product {ID = 2, CategoryID = 5, UnitsInStock = 5, ProductName  = "Kalem", UnitPrice = 35};
+
+            //C# is case sensitive.
+            //Pascal Case  //camelCase
+            ProductManager productManager = new ProductManager();
+            string name = "Göktuğ";
+            productManager.Add();
+            Console.WriteLine(product1.ProductName);
+
+            //int, double, bool - Value Type
+            //arrays, class, abstract class, interface - Reference Type
+        }
+    }
+}
